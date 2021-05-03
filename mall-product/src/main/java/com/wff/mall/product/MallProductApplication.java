29dc339224d7541cb6df9a2df545a58c190475c0
wff0 <1098137961@qq.com>
2021-05-03 12:26:37 +0800

    基本CRUD代码逆向生成

--- mall-product/src/main/java/com/wff/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/wff/mall/product/MallProductApplication.java
@@ -1,9 +1,11 @@
 package com.wff.mall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.wff.mall.product.dao")
 public class MallProductApplication {
 
     public static void main(String[] args) {
